e91bcb0a76fdab51d3e517aee9ee5e2710a26c6a
Manuel Gallardo <121906123+manu-mx@users.noreply.github.com>
2024-05-15 20:39:41 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-def main():
-    # Solicitar nombre y edad al usuario
-    nombre = input("Por favor, ingresa tu nombre: ")
-    edad = input("Por favor, ingresa tu edad: ")
-
-    # Imprimir un mensaje de saludo con el nombre y la edad
-    print(f"Hola, {nombre}! Tienes {edad} años.")
-
-if __name__ == "__main__":
-    main()
+nombre = input("Por favor, ingresa tu nombre: ")
+edad = input("Por favor, ingresa tu edad: ")
+print("Hola", nombre, "Tienes", edad, "años.")
